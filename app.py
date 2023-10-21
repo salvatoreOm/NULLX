bbea2fc5b1e7014f9622096a194100c8f1fd73fc
salvatoreOm <141987998+salvatoreOm@users.noreply.github.com>
2023-10-21 18:08:26 +0000

    changes

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-# Your price comparison functions
-def get_ajio_price(product_name):
-  url = f'https://www.ajio.com/{product_name.replace(" ", "-")}'
+# My price comparison functions
+def get_meesho_price(product_name):
+  url = f'https://www2.hm.com/search?keyword={product_name.replace(" ", "-")}'
   headers = {
-      # Replace with your User-Agent string
       'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                      'AppleWebKit/537.36 (KHTML, like Gecko) '
                      'Chrome/109.0.0.0 Safari/537.36'),
@@ -23,7 +22,6 @@ def get_ajio_price(product_name):
 def get_snapdeal_price(product_name):
   url = f'https://www.snapdeal.com/search?keyword={product_name.replace(" ", "%20")}'
   headers = {
-      # Replace with your User-Agent string
       'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                      'AppleWebKit/537.36 (KHTML, like Gecko) '
                      'Chrome/109.0.0.0 Safari/537.36'),
@@ -36,16 +34,16 @@ def get_snapdeal_price(product_name):
 
 @app.route('/', methods=['GET', 'POST'])
 def price_comparison():
-  ajio_price = None
+  meesho_price = None
   snapdeal_price = None
 
   if request.method == 'POST':
     product_name = request.form.get('product_name')
-    ajio_price = get_ajio_price(product_name)
+    meesho_price = get_meesho_price(product_name)
     snapdeal_price = get_snapdeal_price(product_name)
 
   return render_template('home.html',
-                         ajio_price=ajio_price,
+                         meesho_price=meesho_price,
                          snapdeal_price=snapdeal_price)
 
 
