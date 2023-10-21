dfebd2a2a52a0763077b16cc47a98bb9197e9c03
salvatoreOm <141987998+salvatoreOm@users.noreply.github.com>
2023-10-21 16:48:56 +0000

    Made some changes in the html and created the logic of Python,Final Prototype

--- app.py
+++ app.py
@@ -1,12 +1,53 @@
-from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-  return render_template('home.html')
+# Your price comparison functions
+def get_ajio_price(product_name):
+  url = f'https://www.ajio.com/{product_name.replace(" ", "-")}'
+  headers = {
+      # Replace with your User-Agent string
+      'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                     'AppleWebKit/537.36 (KHTML, like Gecko) '
+                     'Chrome/109.0.0.0 Safari/537.36'),
+  }
+  response = requests.get(url, headers=headers)
+  soup = BeautifulSoup(response.text, 'html.parser')
+  price = soup.find('span', {'class': 'pdp-price'})
+  return price.text.strip() if price else 'N/A'
 
 
-if __name__ == "__main__":
+def get_snapdeal_price(product_name):
+  url = f'https://www.snapdeal.com/search?keyword={product_name.replace(" ", "%20")}'
+  headers = {
+      # Replace with your User-Agent string
+      'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                     'AppleWebKit/537.36 (KHTML, like Gecko) '
+                     'Chrome/109.0.0.0 Safari/537.36'),
+  }
+  response = requests.get(url, headers=headers)
+  soup = BeautifulSoup(response.text, 'html.parser')
+  price = soup.find('span', {'class': 'lfloat product-price'})
+  return price.text.strip() if price else 'N/A'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def price_comparison():
+  ajio_price = None
+  snapdeal_price = None
+
+  if request.method == 'POST':
+    product_name = request.form.get('product_name')
+    ajio_price = get_ajio_price(product_name)
+    snapdeal_price = get_snapdeal_price(product_name)
+
+  return render_template('home.html',
+                         ajio_price=ajio_price,
+                         snapdeal_price=snapdeal_price)
+
+
+if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
