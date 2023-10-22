f9da35338fba4e3a3725093615169a8f6894923c
salvatoreOm <141987998+salvatoreOm@users.noreply.github.com>
2023-10-22 10:58:19 +0000

    Added Walmart

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 # My price comparison functions
+def get_walmart_price(product_name):
+  url = f'https://www.walmart.com/search?q={product_name.replace(" ", "+")}'
+  headers = {
+      # Replace with your User-Agent string
+      'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+  }
+  response = requests.get(url, headers=headers)
+  soup = BeautifulSoup(response.content, 'html.parser')
+  soup1 = BeautifulSoup(soup.prettify(), 'html.parser')
+  price = soup1.find('div', class_='mr1 mr2-xl b black lh-copy f5 f4-l')
+  return price.text.strip() if price else 'N/A'
 
 
 def get_flipkart_price(product_name):
@@ -31,7 +44,7 @@ def get_amazon_price(product_name):
   response = requests.get(url, headers=headers)
   soup = BeautifulSoup(response.content, 'html.parser')
   soup1 = BeautifulSoup(soup.prettify(), 'html.parser')
-  price = soup1.find('span', class_='a-offscreen')
+  price = soup1.find('span', class_='a-price')
   return price.text.strip() if price else 'N/A'
 
 
@@ -50,24 +63,23 @@ def get_snapdeal_price(product_name):
 
 @app.route('/', methods=['GET', 'POST'])
 def price_comparison():
-  # meesho_price = None
+  walmart_price = None
   snapdeal_price = None
   amazon_price = None
   flipkart_price = None
 
   if request.method == 'POST':
     product_name = request.form.get('product_name')
-    # meesho_price = get_meesho_price(product_name)
+    walmart_price = get_walmart_price(product_name)
     snapdeal_price = get_snapdeal_price(product_name)
     amazon_price = get_amazon_price(product_name)
     flipkart_price = get_flipkart_price(product_name)
 
-  return render_template(
-      'home.html',
-      # meesho_price=meesho_price,
-      snapdeal_price=snapdeal_price,
-      amazon_price=amazon_price,
-      flipkart_price=flipkart_price)
+  return render_template('home.html',
+                         walmart_price=walmart_price,
+                         snapdeal_price=snapdeal_price,
+                         amazon_price=amazon_price,
+                         flipkart_price=flipkart_price)
 
 
 if __name__ == '__main__':
