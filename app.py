5cd37f7276ca60a7faee70222bdf8edf877585fa
salvatoreOm <141987998+salvatoreOm@users.noreply.github.com>
2023-10-22 11:48:29 +0000

    Made Some Random Changes

--- app.py
+++ app.py
@@ -21,17 +21,33 @@ def get_walmart_price(product_name):
 
 
 def get_Croma_price(product_name):
-  url = f'https://www.croma.com/searchB?q={product_name.replace(" ", "%20")}'
+  url = f'https://www.croma.com/search/?text={product_name.replace(" ", "%20")}'
   headers = {
       # Replace with your User-Agent string
       'User-Agent':
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
   }
-  response = requests.get(url, headers=headers)
-  soup = BeautifulSoup(response.content, 'html.parser')
-  soup1 = BeautifulSoup(soup.prettify(), 'html.parser')
-  price = soup1.find('div', class_='amount')
-  return price.text.strip() if price else 'N/A'
+  try:
+    # Send a GET request to the URL with custom headers
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+      # Parse the HTML content of the page
+      soup = BeautifulSoup(response.text, 'html.parser')
+
+      # Find the price element on the page
+      price = soup.find('span', {'class': 'amount'})
+
+      # Extract the price text
+      if price:
+        return price.text.strip()
+      else:
+        return 'N/A'  # Price not found
+    else:
+      return 'N/A'  # Request was unsuccessful
+  except Exception as e:
+    return 'Error: ' + str(e)
 
 
 def get_amazon_price(product_name):
